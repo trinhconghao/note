2f00c4fb981ce79598d7b41e8360443798c713be
Trịnh Công Hào <conghaolp10a111@gmail.com>
2025-04-09 16:45:12 +0700

    f21

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // eslint: {
-  //   // ⚠️ Bỏ qua lỗi ESLint khi build
-  //   ignoreDuringBuilds: true,
-  // },
+  eslint: {
+    // ⚠️ Bỏ qua lỗi ESLint khi build
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
